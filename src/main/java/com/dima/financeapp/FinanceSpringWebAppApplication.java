5f2bb8cf087ea0ae22d2e94623a138e1f3ce577d
Dmitry Turov <dmitry.turov374@gmail.com>
2021-05-21 00:13:19 +0300

    first commit

---
+++ src/main/java/com/dima/financeapp/FinanceSpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.dima.financeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSpringWebAppApplication.class, args);
+	}
+
+}
